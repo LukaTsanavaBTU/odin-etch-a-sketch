5bf41d7f573907c9bb53c5299abba6283feacacb
LukaTsanavaBTU <luka.tsanava.1@btu.edu.ge>
2024-03-12 16:35:15 +0400

    Add logic for changing drawing space dimensions without checking for input vadility

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,38 @@
 window.ondragstart = ()=> { return false; } 
 
-const divMain = document.querySelector("div");
-divMain.addEventListener("contextmenu", (event) => {
-    event.preventDefault();
-})
+const changeDimensionsButton = document.querySelector("button");
+changeDimensionsButton.addEventListener("click", () => {
+    let newDimensions = prompt("Enter the new dimensions:");
+    drawGrid(+newDimensions);
+});
 
-for (let i = 0; i < 16; i++) {
-    const divContainer = document.createElement("div");
-    divContainer.classList.add("div-container");
-    divMain.appendChild(divContainer);
-    for (let j = 0; j < 16; j++) {
-        const divPixel = document.createElement("div");
-        divPixel.classList.add("div-pixel");
-        divPixel.classList.add("inactive");
-        divPixel.addEventListener("mouseenter", onMouseEnter);
-        divPixel.addEventListener("mousedown", onMouseEnter);
-        divContainer.appendChild(divPixel);
+function drawGrid(dimensions) {
+    const oldDiv = document.querySelector(".div-main");
+    if (oldDiv) {
+        document.body.removeChild(oldDiv);
     }
+    divMain = document.createElement("div");
+    divMain.classList.add("div-main");
+    divMain.addEventListener("contextmenu", (event) => {
+        event.preventDefault();
+    })    
+    document.body.appendChild(divMain);
+    for (let i = 0; i < dimensions; i++) {
+        const divContainer = document.createElement("div");
+        divContainer.classList.add("div-container");
+        divContainer.style.setProperty("height", `${100/dimensions}%`);
+        divMain.appendChild(divContainer);
+        for (let j = 0; j < dimensions; j++) {
+            const divPixel = document.createElement("div");
+            divPixel.classList.add("div-pixel");
+            divPixel.classList.add("inactive");
+            divPixel.style.setProperty("width", `${100/dimensions}%`);
+            divPixel.addEventListener("mouseenter", onMouseEnter);
+            divPixel.addEventListener("mousedown", onMouseEnter);
+            divContainer.appendChild(divPixel);
+        }
+    }
+    
 }
 
 function onMouseEnter(event) {
@@ -28,3 +44,6 @@ function onMouseEnter(event) {
         event.target.classList.add("inactive");
     }
 }
+
+drawGrid(32);
+
