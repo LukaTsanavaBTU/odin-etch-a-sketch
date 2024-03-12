8880780759510d4287ea6be3e7b2aea977ac9fa2
LukaTsanavaBTU <luka.tsanava.1@btu.edu.ge>
2024-03-12 16:11:15 +0400

    Add logic for adding and removing pixels

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,10 @@
+window.ondragstart = ()=> { return false; } 
+
 const divMain = document.querySelector("div");
+divMain.addEventListener("contextmenu", (event) => {
+    event.preventDefault();
+})
+
 for (let i = 0; i < 16; i++) {
     const divContainer = document.createElement("div");
     divContainer.classList.add("div-container");
@@ -6,6 +12,19 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const divPixel = document.createElement("div");
         divPixel.classList.add("div-pixel");
+        divPixel.classList.add("inactive");
+        divPixel.addEventListener("mouseenter", onMouseEnter);
+        divPixel.addEventListener("mousedown", onMouseEnter);
         divContainer.appendChild(divPixel);
+    }
+}
+
+function onMouseEnter(event) {
+    if (event.buttons === 1) {
+        event.target.classList.remove("inactive");
+        event.target.classList.add("active");
+    } else if (event.buttons === 2) {
+        event.target.classList.remove("active")
+        event.target.classList.add("inactive");
     }
 }
